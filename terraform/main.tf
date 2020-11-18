c73f496e39f7049096110aaa074055c814efe674
Paul Walkington <paul.walkington@infinityworks.com>
2020-11-18 11:30:45 +0000

    APMSPII-403 - changed name to example-test

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,14 +13,14 @@ terraform {
 }
 
 
-module "{{ config.meta.service_name }}" {
+module "example-test" {
   source                   = "github.com/NHSDigital/api-platform-service-module"
-  name                     = "{{ config.meta.service_name }}"
+  name                     = "example-test"
   path                     = "{{ config.meta.service_base_path }}"
   apigee_environment       = var.apigee_environment
   proxy_type               = (var.force_sandbox || length(regexall("sandbox", var.apigee_environment)) > 0) ? "sandbox" : "live"
   namespace                = var.namespace
   make_api_product         = !(length(regexall("sandbox", var.apigee_environment)) > 0)
-  api_product_display_name = length(var.namespace) > 0 ? "{{ config.meta.service_name }}${var.namespace}" : "{{ config.meta.product_display_name }}"
+  api_product_display_name = length(var.namespace) > 0 ? "example-test${var.namespace}" : "{{ config.meta.product_display_name }}"
   api_product_description  = "{{ config.meta.product_description}}"
 }

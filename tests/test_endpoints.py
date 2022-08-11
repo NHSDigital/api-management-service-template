503616ee9b3743eb7b21d1354f7e59cde7d7d8fe
jslb <jodiebarnsley27@gmail.com>
2022-08-11 16:15:56 +0100

    APM-3218 uplift tests to pytest apim plugin

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,70 +1,46 @@
+"""
+See
+https://github.com/NHSDigital/pytest-nhsd-apim/blob/main/tests/test_examples.py
+for more ideas on how to test the authorization of your API.
+"""
+import requests
 import pytest
-from api_test_utils.oauth_helper import OauthHelper
-from api_test_utils.apigee_api_apps import ApigeeApiDeveloperApps
-from api_test_utils.apigee_api_products import ApigeeApiProducts
 
 
-class TestEndpoints:
+@pytest.mark.smoketest
+def test_ping(nhsd_apim_proxy_url):
+    resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
+    assert resp.status_code == 200
 
-    @pytest.fixture()
-    def app(self):
-        """
-        Import the test utils module to be able to:
-            - Create apigee test application
-                - Update custom attributes
-                - Update custom ratelimits
-                - Update products to the test application
-        """
-        return ApigeeApiDeveloperApps()
 
-    @pytest.fixture()
-    def product(self):
-        """
-        Import the test utils module to be able to:
-            - Create apigee test product
-                - Update custom scopes
-                - Update environments
-                - Update product paths
-                - Update custom attributes
-                - Update proxies to the product
-                - Update custom ratelimits
-        """
-        return ApigeeApiProducts()
+@pytest.mark.smoketest
+def test_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
+    resp = requests.get(
+        f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers
+    )
+    assert resp.status_code == 200
+    # Make some additional assertions about your status response here!
 
-    @pytest.fixture()
-    async def test_app_and_product(self, app, product):
-        """Create a test app and product which can be modified in the test"""
-        await product.create_new_product()
 
-        await app.create_new_app()
+@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level0"})
+def test_app_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
+    assert resp.status_code == 401  # unauthorized
 
-        await product.update_scopes([
-            "urn:nhsd:apim:app:level3:{{ SERVICE_NAME }}",
-            "urn:nhsd:apim:user-nhs-id:aal3:{{ SERVICE_NAME }}"
-        ])
-        await app.add_api_product([product.name])
 
-        yield product, app
+@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
+def test_app_level3(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
+    assert resp.status_code == 200
 
-        await app.destroy_app()
-        await product.destroy_product()
 
-    @pytest.fixture()
-    async def get_token(self, test_app_and_product):
-        """Call identity server to get an access token"""
-        test_product, test_app = test_app_and_product
-        oauth = OauthHelper(
-            client_id=test_app.client_id,
-            client_secret=test_app.client_secret,
-            redirect_uri=test_app.callback_url
-            )
-        token_resp = await oauth.get_token_response(grant_type="authorization_code")
-        assert token_resp["status_code"] == 200
-        return token_resp['body']
-
-    def test_user_restricted(self, get_token):
-        """
-        In here you can add tests which call your proxy
-        You can use the 'get_token' fixture to call the proxy with a access token
-        """
-        pass
+@pytest.mark.nhsd_apim_authorization(
+    {
+        "access": "healthcare_worker",
+        "level": "aal3",
+        "login_form": {"username": "656005750104"},
+    }
+)
+def test_cis2_aal3(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
+    assert resp.status_code == 200
